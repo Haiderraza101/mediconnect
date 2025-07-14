e9001b34bbed6771e78cad2f8bf455d57d034cfc
Haider Raza Kazmi <haiderraza7516@gmail.com>
2025-07-14 17:53:07 +0500

    Added environment variables and configured dotenv

--- backend/utils/databaseutil.js
+++ backend/utils/databaseutil.js
@@ -1,10 +1,11 @@
-const mysql= require('mysql2');
+require("dotenv").config(); // make sure this line is at the top
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host:'localhost',
-  user:'root',
-  password:'haider7516',
-  database:'mediconnect'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool.promise();
